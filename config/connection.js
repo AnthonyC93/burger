858f2b1f2c4db3cd45d9ac388767e25b58a210bd
acarta <anthonym.cartagena@gmail.com>
2019-02-04 21:08:55 -0500

    added jaws stuff. not sure why not deploying

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,18 @@
 //setup the code to connect Node to MySQL.
-//export the connectin
+//export the connection
 var mysql = require('mysql');
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "",
-    database: "burgers_db"
-});
+if(process.env.JAWSDB_URL){
+    var connection = mysql.createConnection(process.env.JAWSDB_URL);
+}else{
+    var connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "",
+        database: "burgers_db"
+    });
+}
 
 connection.connect(function(err){
     if(err)throw err;
